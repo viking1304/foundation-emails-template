c76901cc39b3f4c974a13ca7924d4c66865a5f3b
viking1304 <viking1304@gmail.com>
2024-01-24 11:49:48 +0100

    Remove Node 10 fix

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -12,9 +12,6 @@ import path     from 'path';
 import merge    from 'merge-stream';
 import beep     from 'beepbeep';
 
-// node v10 fix
-import 'globalthis/auto';
-
 const $ = plugins();
 
 var dartSass = require('gulp-sass')(require('sass'));
